a4b3c5ec8f56e1f2977f0af554a76bb37facc17b
SmemainLi <877742567@qq.com>
2018-09-17 22:02:19 +0800

    vue-loader VueLoaderPlugin  css-loader & mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
 const path = require('path')
+const {
+    VueLoaderPlugin
+} = require('vue-loader')
 
 module.exports = {
+    /* mode: 'production', */
+    mode: 'development',
     entry: path.join(__dirname, 'src/index.js'),
     output: {
         filename: 'bundle.js',
         path: path.join(__dirname, 'dist')
-    }
+    },
+    module: {
+        rules: [{
+                test: /.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
 }
