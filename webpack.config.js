c14b8f70485b4cd987ea85d242557b4d4dabb841
SmemainLi <877742567@qq.com>
2018-09-18 21:53:55 +0800

    css & stylus loader

--- webpack.config.js
+++ webpack.config.js
@@ -13,12 +13,30 @@ module.exports = {
     },
     module: {
         rules: [{
-                test: /.vue$/,
+                test: /\.vue$/,
                 loader: 'vue-loader'
             },
             {
-                test: /.css$/,
+                test: /\.css$/,
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.styl$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'stylus-loader'
+                ]
+            },
+            {
+                test: /\.(gif|jpg|jpeg|png|svg)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 1024,
+                        name: '[name]-aaa.[ext]'
+                    }
+                }]
             }
         ]
     },
